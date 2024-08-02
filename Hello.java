a8b2e358becce3065365fc47ce3f9b17b6d189c9
LetoBad <Let0.die9962@gmail.com>
2024-08-02 20:29:31 -0300

    version 1 meodo

--- Hello.java
+++ Hello.java
@@ -5,5 +5,6 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("alou mundinho!");
         System.out.println("12312321");
+        System.out.println("eitaaa ");
     }
 }
